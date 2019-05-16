0cb1cef870d14c4a0a8a74d8d53338c7854f2f1c
Stozzy <sagoldyn@gmail.com>
2019-05-15 23:03:34 -0400

    initial commit
    Signed-off-by: Stozzy <sagoldyn@gmail.com>

---
+++ canvas.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("#canvas");
+    const context = canvas.getContext("2d");
+
+    //Resizing canvas
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    //variables
+    let painting = false;
+
+    function startPos(e) {
+        painting = true;
+        draw(e);
+    }
+
+    function stopPos() {
+        painting = false;
+        clearCanvas()
+        //context.beginPath();
+    }
+
+    function draw(e) {
+        if (!painting) return;
+
+        context.lineWidth = 10;
+        context.lineCap = 'round';
+        context.lineTo(e.clientX, e.clientY);
+
+        context.stroke();
+        context.beginPath();
+        context.moveTo(e.clientX, e.clientY); //makes drawing smoother
+    }
+    
+    function clearCanvas() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+    }
+
+    //event listeners
+    canvas.addEventListener('mousedown', startPos);
+    canvas.addEventListener('mouseup', stopPos);
+    canvas.addEventListener('mousemove', draw);
+    
+    console.log(canvas);
+});
